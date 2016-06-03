6b23d202adf47d1edaca58786624bf7138738ecb
johnreed <johnreedlol@ufl.edu>
2016-06-03 18:59:52 -0400

    getting output of compilation works

--- src/main/java/com/example/JavaMain.java
+++ src/main/java/com/example/JavaMain.java
@@ -35,7 +35,11 @@ sbt "clean" "set scalacOptions in ThisBuild ++= Seq(\"-Xprint:parser\")" "compil
             }
             System.out.println("Try #2 - using Runtime.getRuntime().exec");
             // Process p = Runtime.getRuntime().exec("ls -l"); // this works
-            Process proc = Runtime.getRuntime().exec("javac -help"); // this doesn't
+            //Process proc = Runtime.getRuntime().exec("javac -help"); //
+            // sbt "clean" "set scalacOptions in ThisBuild ++= Seq(\"-Xprint:parser\")" "compile" "exit"
+            Process proc = Runtime.getRuntime().exec(new String[] {"sbt", "clean",
+            "set scalacOptions in ThisBuild ++= Seq(\"-Xprint:parser\")",
+            "compile", "exit"});
             BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
             BufferedReader inErr = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
             {
@@ -49,7 +53,7 @@ sbt "clean" "set scalacOptions in ThisBuild ++= Seq(\"-Xprint:parser\")" "compil
             {
                 String line = null;
                 while ((line = inErr.readLine()) != null) {
-                    System.out.println(line);
+                    // System.out.println(line);
                 }
                 System.out.println("Done2");
                 inErr.close();
