a5c7c15c2f6544b1540820fd6f941fa7b18a5d10
johnreed <johnreedlol@ufl.edu>
2016-06-04 15:56:30 -0400

    preparing regexs

--- src/main/java/com/example/JavaMain.java
+++ src/main/java/com/example/JavaMain.java
@@ -3,10 +3,43 @@ package com.example;
     import java.io.*;
     import java.util.*;
     import info.collaboration_station.utilities.*;
+    import java.util.ArrayList;
+    import java.util.List;
+    import java.util.regex.*;
 
     public class JavaMain {
-        public static void main(String args[]) throws Exception {
 
+        public static void capture() {
+            String input = "I have a cat-  -, but I like my dog better.";
+
+            Pattern p = Pattern.compile("(mouse|cat-  -|dog|wolf|bear|human)");
+            Matcher m = p.matcher(input);
+
+            List<String> animals = new ArrayList<String>();
+            while (m.find()) {
+                System.out.println("Found a " + m.group() + ".");
+                animals.add(m.group());
+            }
+        }
+
+        public static void capture2() {
+            String input = "User clientId=23421. Some more text clientId=33432. This clientNum=100";
+
+            Pattern p = Pattern.compile("(clientId=)(\\d+)");
+            Matcher m = p.matcher(input);
+
+            StringBuffer result = new StringBuffer();
+            while (m.find()) {
+                System.out.println("Masking: " + m.group(2));
+                m.appendReplacement(result, m.group(1) + "***masked***");
+            }
+            m.appendTail(result);
+            System.out.println(result);
+        }
+
+        public static void main(String args[]) throws Exception {
+            capture();
+            capture2();
 /*
 // This is the command we are going to be running:
 
@@ -17,47 +50,71 @@ sbt "clean" "set scalacOptions in ThisBuild ++= Seq(\"-Xprint:parser\")" "compil
 " // *.scala"
 
 */
+            List<String> input = new ArrayList<String>();
+            input.add("123-45-6789"); // match
+            input.add("9876-5-4321");
+            input.add("987-65-4321 (attack)");
+            input.add("987-65-4321 ");
+            input.add("192-83-7465"); // match
+            input.add("192--7465");
+            input.add("192--");
+            input.add("192");
+            input.add("7465");
+            input.add("987654321"); // match
 
-            System.out.println("Try #1 - using ProcessBuilder");
-            {
-                ProcessBuilder pb = new ProcessBuilder("javac", "-help");
-                Map<String, String> env = pb.environment();
-                env.put("VAR1", "myValue");
-                env.remove("OTHERVAR");
-                env.put("VAR2", env.get("VAR1") + "suffix");
-                pb.directory(null); // use the working directory of the current
-                File log = new File("log");
-                pb.redirectErrorStream(true);
-                pb.redirectOutput(ProcessBuilder.Redirect.appendTo(log));
-                Process p = pb.start();
-                assert pb.redirectInput() == ProcessBuilder.Redirect.PIPE;
-                assert pb.redirectOutput().file() == log;
-                assert p.getInputStream().read() == -1;
-            }
-            System.out.println("Try #2 - using Runtime.getRuntime().exec");
-            // Process p = Runtime.getRuntime().exec("ls -l"); // this works
-            //Process proc = Runtime.getRuntime().exec("javac -help"); //
-            // sbt "clean" "set scalacOptions in ThisBuild ++= Seq(\"-Xprint:parser\")" "compile" "exit"
-            Process proc = Runtime.getRuntime().exec(new String[] {"sbt", "clean",
-            "set scalacOptions in ThisBuild ++= Seq(\"-Xprint:parser\")",
-            "compile", "exit"});
-            BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-            BufferedReader inErr = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
-            {
-                String line = null;
-                while ((line = in.readLine()) != null) {
-                    System.out.println(line);
+            // ., \\d, \\D, \\s, \\S, \\w, \\W
+            /*
+                .  	Dot, any character (may or may not match line terminators, read on)
+                \d  	A digit: [0-9]
+                \D  	A non-digit: [^0-9]
+                \s  	A whitespace character: [ \t\n\x0B\f\r]
+                \S  	A non-whitespace character: [^\s]
+                \w  	A word character: [a-zA-Z_0-9]
+                \W  	A non-word character: [^\w]
+             */
+            // *, +, ?, {n}, {n,}, {n,m}
+            /*
+                *      Match 0 or more times
+                +      Match 1 or more times
+                ?      Match 1 or 0 times
+                {n}    Match exactly n times
+                {n,}   Match at least n times
+                {n,m}  Match at least n but not more than m times
+             */
+            /*
+            \   	Escape the next meta-character (it becomes a normal/literal character)
+            ^   	Match the beginning of the line
+            .   	Match any character (except newline)
+            $   	Match the end of the line (or before newline at the end)
+            |   	Alternation (‘or’ statement)
+            ()  	Grouping
+            []  	Custom character class
+             */
+
+            String ssnMatcher = "^" + // Match the beginning of the line
+                "(" + // Start grouping
+                "\\d{3}" + // Exactly 3 digits [0-9]
+                "-?" + // A hyphen match 1 or 0 times
+                "\\d{2}" + // Exactly 2 digits [0-9]
+                "-?" + // A hyphen match 1 or 0 times
+                "\\d{4}" + // Exactly 4 digits [0-9]
+                ")" + // end grouping
+            "$"; // Match the end of the line
+
+            for (String ssn : input) {
+                if (ssn.matches("^(\\d{3}-?\\d{2}-?\\d{4})$")) {
+                    System.out.println("Found good SSN: " + ssn);
                 }
-                System.out.println("Done1");
-                in.close();
             }
-            {
-                String line = null;
-                while ((line = inErr.readLine()) != null) {
-                    // System.out.println(line);
-                }
-                System.out.println("Done2");
-                inErr.close();
+
+            String matcher2 = "I lost my " +
+                "\\w+"; // 1 or more word characters [a-zA-Z_0-9]
+
+            if ("I lost my 149regVRT56rt".matches(matcher2)) {
+                System.out.println("Found good");
             }
+
+            String matcher3 = "I lost my:? (val|var)";
+
         }
     }
--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -2,10 +2,79 @@ package com.example
 
 import java.io._
 import java.util._
+import info.collaboration_station.utilities._
 
 object Hello {
   def main(args: Array[String]): Unit = {
     val toPrint = "Hello, world!"
     println(toPrint)
+
+    val proc: Process = Runtime.getRuntime.exec(Array[String]("sbt", "clean", "set scalacOptions in ThisBuild ++= Seq(\"-Xprint:parser\")", "compile", "exit"))
+    val in: BufferedReader = new BufferedReader(new InputStreamReader(proc.getInputStream))
+    var line: String = null
+
+    // start line
+    // optional leading characters
+    // leading whitespace [1 or more]
+    // double forward slash
+    // one whitespace character
+    // file name (one or more characters)
+    // .java
+    // end of line
+    val fileNameExtractorRegEx =
+      """
+         |^(
+         |.*
+         |\s+
+         |//
+         |\s
+         |fileName=(.+)
+         |.java
+         |)$
+       """.stripMargin
+
+    // extracts declarations such as " val cat_!: Cat = makeCat()" or "Foo.bar();var     <><: :Int = 7"
+    // or "/*comment*/ var cat: CAT_<>< = makeCat()"
+    val declarationExtractorRegEx =
+      // start line
+      // whitespace or comments than semicolon then whitespace or comments then whitespace
+      // var or val
+      // optional whitespace
+      // variable name with arbitrary characters (not necessarily lowercase)
+      // optional whitespace
+      // colon
+      // optional whitespace
+      // An upper case letter indicating a type
+      // subsequent optional letters or symbols following it (non-whitespace)
+      // optional whitespace
+      // and equal sign
+      // arbitrary subsequent characters (including but not limited to whitespace)
+      // end of line
+      """
+        |^(
+        |(\s*|.*;\s*|.*\s+)
+        |(val |var )
+        |\s*
+        |varName=(.+)
+        |\s*
+        |:
+        |\s*
+        |typeName=([A-Z]\S+)
+        |\s*
+        |=
+        |.*
+        |)$
+      """.stripMargin
+    /*
+    val declarationExtractorRegEx = raw"^(" +
+      raw"\s*" +
+      raw"" +
+      raw"$"
+    */
+    while ({line = in.readLine; line} != null) {
+      System.out.println(line)
+    }
+    System.out.println("Done1")
+    in.close
   }
 }
