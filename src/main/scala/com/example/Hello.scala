b8728aa0f8786fe4ff0c6eececda799843dcaf39
johnreed <johnreedlol@ufl.edu>
2016-06-06 02:24:56 -0400

    refactoring part 2

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -39,9 +39,17 @@ object Hello {
     FileFinder.setMySearchDepth(20)
 
     type FiveStrings = (String, String, String, String, String)
-    def matchInDesugaredCode(reader: BufferedReader, parsedDeclaration: FiveStrings): Unit = {
+    /** Match the parsed declaration from the source code with the matching declaration in
+      * the desugared code (read in with the reader) to get a typed declaration.
+      * @param reader Reads in the desugared code to match against
+      * @param parsedDeclaration the parsed declaration from source to match against
+      * the desugared code.
+      * @return The parsedDeclaration with a type inserted or None
+      */
+    def getTypedDeclaration(reader: BufferedReader, parsedDeclaration: FiveStrings): Option[String] = {
       val (leftSide, filler, valOrVar, varName, rightSide) = parsedDeclaration
       var matchesDeclaration = false
+      var toReturn: Option[String] = None
       while (matchesDeclaration == false && line != null) {
         line = reader.readLine
         Debug.trace("Starting Loop 3!!!")
@@ -49,6 +57,7 @@ object Hello {
           case RegEx.TypedDesugaredDeclExtractor(boilerFiller, boilerValOrVar, boilerVarName, boilerRefType) =>
             if(boilerValOrVar.equals(valOrVar) && boilerVarName.equals(varName)) {
               Debug.trace(s"$leftSide$rightSide| matched: $boilerValOrVar $boilerVarName: $boilerRefType = ...;")
+              toReturn = Some(s"$leftSide: $boilerRefType$rightSide") // put in type
               matchesDeclaration = true // exit
             } else {
               Debug.trace(s"$leftSide$rightSide| failed to match: $boilerValOrVar $boilerVarName: $boilerRefType = ...;")
@@ -57,6 +66,7 @@ object Hello {
             if(boilerDecl.equals(valOrVar) && boilerVarName.equals(varName)) {
               // they matched, but no need to insert a type
               Debug.trace(s"$leftSide$rightSide| matched: $boilerDecl $boilerVarName = ...;")
+              toReturn = None // None can also be used if no type was inserted in desugared code
               matchesDeclaration = true // exit
             } else {
               Debug.trace(s"$leftSide$rightSide| failed to match: $boilerDecl $boilerVarName = ...;")
@@ -64,37 +74,49 @@ object Hello {
           case unmatchedLine  =>
             System.err.println(s"Was searching for: |$leftSide$rightSide| in boilerplatey code")
             System.err.println(unmatchedLine + Pos())
+            toReturn = None // just to be explicit
         }
       }
+      toReturn
     }
 
-    def iterateThroughSourceFile(file: String): Unit = {
-      var fileLine: String = ""
+    /** Iterate through the source file in the SBT project with the given name.
+      * Find each of its declarations and type them.
+      * @param fileName the file name of the given file in the SBT project
+      * @return the source file as one long strings with types put into the declarations.
+      */
+    def iterateThroughSourceFile(fileName: String): String = {
+      var line: String = ""
       var linesInFile: String = ""
-      val br: BufferedReader = new BufferedReader(new FileReader(file))
+      val fileReader: BufferedReader = new BufferedReader(new FileReader(fileName))
       try {
         // then search through source code
-        while ( {fileLine = br.readLine; fileLine} != null) {
+        while ( {line = fileReader.readLine; line} != null) {
           Debug.trace("Starting Loop 2!!!")
-          fileLine match {
+          line match {
             case RegEx.SourceDeclExtractor(leftSide, filler, valOrVar, varName, rightSide) =>
               Debug.trace(s"Matched $leftSide$rightSide. In source file (No boilerplate).")
-              matchInDesugaredCode(
+              val toAppend: Option[String] = getTypedDeclaration(
                 reader = in,
                 parsedDeclaration = (leftSide, filler, valOrVar, varName, rightSide)
               )
+              toAppend match {
+                case Some(typedDeclaration) => linesInFile += typedDeclaration + File.separator
+                case None => linesInFile += line + File.separator
+              }
               Debug.trace(s"$leftSide$rightSide is defined. Out of while loop 3")
-            case unmatchedLine =>
-              linesInFile += unmatchedLine + File.separator // append without modification
+            case _ =>
+              linesInFile += line + File.separator // append without modification
           }
           // if the line is a declaration, insert the type (increment the desugared file until you get a match)
         }
       } finally {
-        if (br != null) {
-          br.close()
+        if (fileReader != null) {
+          fileReader.close()
           Debug.trace("closed br")
         }
       }
+      linesInFile
     }
 
     // first search through desugared code
@@ -110,7 +132,7 @@ object Hello {
             case Some(path) =>
               Debug.trace("Some path: " + path.toString)
               val currentFile = path.toString
-              iterateThroughSourceFile(file = currentFile)
+              iterateThroughSourceFile(fileName = currentFile)
             //JavaMain.handleFile(path)
             case None => Tester.killApplication(
               s"This file $fileName was supposed to exist, but could not be found in " + FileFinder.WORKING_DIRECTORY
